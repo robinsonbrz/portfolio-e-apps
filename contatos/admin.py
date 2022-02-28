ab2457373167414fd15a062ad9244bf513f94c7d
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-02-28 14:12:43 -0300

    Adição de campos a area administrativa e campos personalizados

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ("id","nome", "sobrenome", "telefone", "email", "data_criacao", "descricao","categoria")
+    list_display_links = ("id","nome", "sobrenome")
+    list_filter = ("nome", "sobrenome")
+    list_per_page = 20                                  # limita a quantidade de contatos por pagina
+    search_fields = ("nome", "sobrenome", "telefone")   # Insere campos de pesquisa
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -7,6 +7,8 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -16,3 +18,6 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome

6f6109d54419642f96cb557db53e27d7ef7fe973
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-03-01 12:26:52 -0300

    levantando erro 404 view

--- contatos/views.py
+++ contatos/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
-    return render(request, 'contatos/index.html', {
-         'contatos': contatos
-    })
+    try: 
+        contatos = Contato.objects.all()
+        return render(request, 'contatos/index.html', {
+            'contatos': contatos
+        })
+    except:
+        raise Http404
 
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
     return render(request, 'contatos/ver_contato.html', {
-         'contato': contato
+        'contato': contato
     })

f1fb6a4771c4a9fa325f8ea1b81cebcf9579a7c8
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-03-01 16:50:50 -0300

    Filtrando e ordenando contatos na view

--- contatos/views.py
+++ contatos/views.py
@@ -6,7 +6,9 @@ from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 25)  # Show 25 contacts per page.
     page_number = request.GET.get('page')
     contatos = paginator.get_page(page_number)
@@ -17,6 +19,9 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+    if not contato.mostrar:   # evita forçar páginas de número não permitidas mostrar
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })

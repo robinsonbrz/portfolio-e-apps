08abcb8b179409127bcd01181975d7f815dbbff3
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-03-01 21:26:02 -0300

    Pesquisa e rodapé

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,6 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
@@ -24,4 +26,26 @@ def ver_contato(request, contato_id):
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo') 
+    if termo is None or not termo:
+        raise Http404()
+
+    campos = Concat('nome', Value(' ') , 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo)|Q(telefone__icontains=termo)
+     ).filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 15)  # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    contatos = paginator.get_page(page_number)
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos,
     })

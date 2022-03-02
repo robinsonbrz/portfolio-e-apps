4774a324b9cea55baa6741f564dc6b5268815699
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-03-02 16:37:33 -0300

    Validação de formulário account

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.core.validators import validate_email
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
@@ -13,9 +15,56 @@ def logout(request):
 
 
 def cadastro(request):
+    if request.method != 'POST':
+        return render(request, 'accounts/cadastro.html')
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha2 = request.POST.get('senha2')
+    if not nome or not sobrenome or not email or not usuario or not \
+                senha or not senha2:
+        messages.error(request, 'Nenhum campo pode estar vazio.')
+        return render(request, 'accounts/cadastro.html')
+
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, 'Email inválido.')
+        return render(request, 'accounts/cadastro.html')        
+    
+    if len(senha) < 6:
+        messages.error(request, 'Senha precisa ter mais que 5 caracteres.')
+        return render(request, 'accounts/cadastro.html')    
+
+    if len(usuario) < 6:
+        messages.error(request, 'Usuario precisa ter mais que 5 caracteres.')
+        return render(request, 'accounts/cadastro.html')            
+
+    if senha != senha2:
+        messages.error(request, 'Senhas digitadas precisam ser iguais.')
+        return render(request, 'accounts/cadastro.html')  
+
+    if User.objects.filter(username=usuario).exists():
+        messages.error(request, 'Usuário já existe.')
+        return render(request, 'accounts/cadastro.html')  
+
+    if User.objects.filter(email=email).exists():
+        messages.error(request, 'Email já existe.')
+        return render(request, 'accounts/cadastro.html')  
+
+    messages.success(request, 'Registrado com sucesso! Agora faça o login.')
+
+    user = User.objects.create_user(username=usuario, email=email,
+                                    password=senha, first_name=nome,
+                                    last_name=sobrenome)
+    user.save()
+    return redirect('login')
+
+    # messages.info(request, 'Nada postado.')
     # print(request.POST)
-    return render(request, 'accounts/cadastro.html')
-
-
+    
+    
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')

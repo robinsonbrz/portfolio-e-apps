69e3d52abac60069b5430813210d29c5cf0e0190
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-03-29 20:00:57 -0300

    Pagina inicial, portfolio, extends, base html

--- portapp/urls.py
+++ portapp/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'portapp'
 urlpatterns = [
-    path('', views.port_index, name='port_index'),
+    path('', views.inicio, name='inicio'),
+    path('about/', views.about, name='about'),
+    path('expertise/', views.expertise, name='expertise'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('contato/', views.contato, name='contato'),
 ]
--- portapp/views.py
+++ portapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 
-def port_index(request):
+def inicio(request):
     return render(request, 'portapp/index.html')
+
+
+def about(request):
+    return render(request, 'portapp/about.html')
+
+
+def expertise(request):
+    return render(request, 'portapp/expertise.html')
+
+
+def portfolio(request):
+    return render(request, 'portapp/portfolio.html')
+
+
+def contato(request):
+    return render(request, 'portapp/contato.html')
